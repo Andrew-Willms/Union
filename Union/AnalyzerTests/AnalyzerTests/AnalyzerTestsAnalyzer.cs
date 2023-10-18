24c4eb612f2450947adadde75a918f8685d1b425
Andrew-Willms <andrew.e.willms@gmail.com>
2023-10-18 01:33:46 -0400

    Began parsing switch statements.

--- Union/AnalyzerTests/AnalyzerTests/AnalyzerTestsAnalyzer.cs
+++ Union/AnalyzerTests/AnalyzerTests/AnalyzerTestsAnalyzer.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Immutable;
-using System.Linq;
+﻿using System;
+using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace AnalyzerTests;
@@ -12,14 +14,13 @@ public class AnalyzerTestsAnalyzer : DiagnosticAnalyzer {
 
 	public const string DiagnosticId = "AnalyzerTests";
 
-	// You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-	// See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
 	private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
 	private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
 	private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
 	private const string Category = "Naming";
 
-	private static readonly DiagnosticDescriptor Rule = new(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+	private static readonly DiagnosticDescriptor Rule = new(DiagnosticId, Title, MessageFormat, Category,
+		DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
 	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
@@ -28,24 +29,35 @@ public class AnalyzerTestsAnalyzer : DiagnosticAnalyzer {
 		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
 		context.EnableConcurrentExecution();
 
-		// TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-		// See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-		context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+		context.RegisterSyntaxNodeAction(AnalyzeSwitchStatement, SyntaxKind.SwitchExpression, SyntaxKind.SwitchStatement);
 	}
 
-	private static void AnalyzeSymbol(SymbolAnalysisContext context) {
+	private static void AnalyzeSwitchStatement(SyntaxNodeAnalysisContext syntaxNodeAnalysisContext) {
 
-		// TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-		INamedTypeSymbol namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+		if (syntaxNodeAnalysisContext.Node is not SwitchStatementSyntax switchStatement) {
+			throw new InvalidOperationException();
+		}
 
-		if (!namedTypeSymbol.Name.ToCharArray().Any(char.IsLower)) {
+		ExpressionSyntax switchStatementExpression = switchStatement.Expression;
+		SemanticModel semanticModel = syntaxNodeAnalysisContext.SemanticModel;
+
+		ITypeSymbol expressionReturnType = semanticModel.GetTypeInfo(switchStatementExpression).Type;
+		INamedTypeSymbol objectType = semanticModel.Compilation.GetTypeByMetadataName(typeof(object).FullName);
+
+		if (!expressionReturnType.Equals(objectType)) {
 			return;
 		}
 
-		// Find just those named type symbols with names containing lowercase letters.
-		// For all such symbols, produce a diagnostic.
-		Diagnostic diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-		context.ReportDiagnostic(diagnostic);
+		SyntaxList<SwitchSectionSyntax> sections = switchStatement.Sections;
+
+		SyntaxList<SwitchLabelSyntax> labels = sections[0].Labels;
+
+		//labels[0].SyntaxTree;
+
+		SyntaxNode node = syntaxNodeAnalysisContext.Node;
+
+		Compilation compilation = syntaxNodeAnalysisContext.Compilation;
+
 	}
 
 }
