fa3bbf81987099ef823fdbffeb86206e11c26b01
Andrew-Willms <andrew.e.willms@gmail.com>
2023-10-22 15:01:54 -0400

    Added xml documentation

--- Union/RoughWork/Program.cs
+++ Union/RoughWork/Program.cs
@@ -9,3 +9,5 @@ Console.WriteLine("Hello, World!");
 Union<int, double> test = 2;
 
 Console.WriteLine(test);
+
+test.NativeSwitch();
--- Union/Union/UnionT2.cs
+++ Union/Union/UnionT2.cs
@@ -33,7 +33,11 @@ public class Union<T1, T2> : IEquatable<Union<T1, T2>> {
 
 
 
-	// TODO: add docs
+	/// <summary>
+	/// Performs an action depending on the internal type of the union.
+	/// </summary>
+	/// <param name="action1">The action to perform if the internal type of the union is <see cref="T1"/>.</param>
+	/// <param name="action2">The action to perform if the internal type of the union is <see cref="T2"/>.</param>
 	public void Switch(Action<T1> action1, Action<T2> action2) {
 
 		if (Value1 is not null) {
@@ -49,6 +53,13 @@ public class Union<T1, T2> : IEquatable<Union<T1, T2>> {
 		throw new UnreachableException();
 	}
 
+	/// <summary>
+	/// Invokes a function depending on the internal type of the union and returns the function output.
+	/// </summary>
+	/// <typeparam name="TResult">The output type of the invoked function.</typeparam>
+	/// <param name="function1">The function to invoke if the internal type of the union is <see cref="T1"/></param>
+	/// <param name="function2">The function to invoke if the internal type of the union is <see cref="T2"/></param>
+	/// <returns>The output of the invoked function.</returns>
 	public TResult Match<TResult>(Func<T1, TResult> function1, Func<T2, TResult> function2) {
 
 		if (Value1 is not null) {
@@ -62,6 +73,11 @@ public class Union<T1, T2> : IEquatable<Union<T1, T2>> {
 		throw new UnreachableException();
 	}
 
+	/// <summary>
+	/// Performs an async action depending on the internal type of the union.
+	/// </summary>
+	/// <param name="action1">The async action to perform if the internal type of the union is <see cref="T1"/>.</param>
+	/// <param name="action2">The async action to perform if the internal type of the union is <see cref="T2"/>.</param>
 	public async Task SwitchAsync(Func<T1, Task> action1, Func<T2, Task> action2) {
 
 		if (Value1 is not null) {
@@ -77,6 +93,13 @@ public class Union<T1, T2> : IEquatable<Union<T1, T2>> {
 		throw new UnreachableException();
 	}
 
+	/// <summary>
+	/// Invokes an async function depending on the internal type of the union and returns the function output.
+	/// </summary>
+	/// <typeparam name="TResult">The output type of the invoked async function.</typeparam>
+	/// <param name="function1">The async function to invoke if the internal type of the union is <see cref="T1"/></param>
+	/// <param name="function2">The async function to invoke if the internal type of the union is <see cref="T2"/></param>
+	/// <returns>The output of the invoked async function.</returns>
 	public async Task<TResult> MatchAsync<TResult>(Func<T1, Task<TResult>> function1, Func<T2, Task<TResult>> function2) {
 
 		if (Value1 is not null) {
@@ -90,6 +113,10 @@ public class Union<T1, T2> : IEquatable<Union<T1, T2>> {
 		throw new UnreachableException();
 	}
 
+	/// <summary>
+	/// Returns the internal value of the union as an <see cref="object"/> allowing for pattern matching and use in switch expressions and statements.
+	/// </summary>
+	/// <returns>The internal value of the union as an <see cref="object"/>.</returns>
 	public object NativeSwitch() {
 
 		if (Value1 is not null) {
