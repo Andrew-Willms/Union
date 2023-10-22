720f63b4f8fa35de2e72f9dab0f10feac0b5920c
Andrew-Willms <andrew.e.willms@gmail.com>
2023-10-22 14:11:37 -0400

    Created Union project and class

---
+++ Union/Union/UnionT2.cs
@@ -0,0 +1,141 @@
+﻿using System.Diagnostics;
+
+namespace Union;
+
+
+
+public class Union<T1, T2> : IEquatable<Union<T1, T2>> {
+
+	private readonly T1? Value1;
+	private readonly T2? Value2;
+
+	public static implicit operator Union<T1, T2>(T1 value) => new(value);
+	public static implicit operator Union<T1, T2>(T2 value) => new(value);
+
+	private Union(T1 value) {
+
+		if (value is null) {
+			throw new ArgumentNullException(nameof(value));
+		}
+
+		Value1 = value;
+	}
+
+	private Union(T2 value) {
+
+
+		if (value is null) {
+			throw new ArgumentNullException(nameof(value));
+		}
+
+		Value2 = value;
+	}
+
+
+
+	public void Switch(Action<T1> action1, Action<T2> action2) {
+
+		if (Value1 is not null) {
+			action1(Value1);
+			return;
+		}
+
+		if (Value2 is not null) {
+			action2(Value2);
+			return;
+		}
+
+		throw new UnreachableException();
+	}
+
+	public TResult Match<TResult>(Func<T1, TResult> function1, Func<T2, TResult> function2) {
+
+		if (Value1 is not null) {
+			return function1(Value1);
+		}
+
+		if (Value2 is not null) {
+			return function2(Value2);
+		}
+
+		throw new UnreachableException();
+	}
+
+	public async Task SwitchAsync(Func<T1, Task> action1, Func<T2, Task> action2) {
+
+		if (Value1 is not null) {
+			await action1(Value1);
+			return;
+		}
+
+		if (Value2 is not null) {
+			await action2(Value2);
+			return;
+		}
+
+		throw new UnreachableException();
+	}
+
+	public async Task<TResult> MatchAsync<TResult>(Func<T1, Task<TResult>> function1, Func<T2, Task<TResult>> function2) {
+
+		if (Value1 is not null) {
+			return await function1(Value1);
+		}
+
+		if (Value2 is not null) {
+			return await function2(Value2);
+		}
+
+		throw new UnreachableException();
+	}
+
+
+
+	public bool Equals(Union<T1, T2>? other) {
+
+		if (other is null) {
+			return false;
+		}
+
+		if (Value1 is not null && other.Value1 is not null) {
+			return Value1.Equals(other.Value1);
+		}
+
+		if (Value2 is not null && other.Value2 is not null) {
+			return Value2.Equals(other.Value2);
+		}
+
+		throw new UnreachableException();
+	}
+
+	public override bool Equals(object? @object) {
+
+		if (ReferenceEquals(this, @object)) {
+			return true;
+		}
+
+		return @object is Union<T1, T2> union && Equals(union);
+	}
+
+	public static bool operator ==(Union<T1, T2> left, Union<T1, T2> right) {
+		return left.Equals(right);
+	}
+
+	public static bool operator !=(Union<T1, T2> left, Union<T1, T2> right) {
+		return !(left == right);
+	}
+
+	public override int GetHashCode() {
+
+		if (Value1 is not null) {
+			return Value1.GetHashCode();
+		}
+
+		if (Value2 is not null) {
+			return Value2.GetHashCode();
+		}
+
+		throw new UnreachableException();
+	}
+
+}
