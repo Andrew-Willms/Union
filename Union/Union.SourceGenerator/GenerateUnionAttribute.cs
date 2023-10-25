6f2014eb3308c054336f7db61b07b0963b76f3f2
Andrew-Willms <andrew.e.willms@gmail.com>
2023-10-25 00:21:25 -0400

    added xml docs and a todo

--- Union/Union.SourceGenerator/GenerateUnionAttribute.cs
+++ Union/Union.SourceGenerator/GenerateUnionAttribute.cs
@@ -4,6 +4,9 @@ namespace Union.SourceGenerator;
 
 
 
-// todo add docs
+// todo: link to do the union nuget package once I publish it
+/// <summary>
+/// Add this attribute above a class inheriting from Union to source generate the necessary constructor and implicit conversions operators./>
+/// </summary>
 [AttributeUsage(AttributeTargets.Class, Inherited = false)]
 public class GenerateUnionAttribute : Attribute;
